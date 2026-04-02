1c70fbfe3352eefff7081f614376402681fc43a5
freehij <sip22ssd@gmail.com>
2026-04-02 14:09:37 +0300

    added server merging to mcartifact (fixed #1)

--- mcartifact/src/main/groovy/io/github/freehij/mcartifact/McArtifactPlugin.groovy
+++ mcartifact/src/main/groovy/io/github/freehij/mcartifact/McArtifactPlugin.groovy
@@ -3,6 +3,9 @@ package io.github.freehij.mcartifact
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import groovy.json.JsonSlurper
+import java.util.zip.ZipEntry
+import java.util.zip.ZipFile
+import java.util.zip.ZipOutputStream
 
 class McArtifactExtension {
     String version
@@ -11,32 +14,62 @@ class McArtifactExtension {
 
 class McArtifactPlugin implements Plugin<Project> {
     void apply(Project project) {
-        def extension = project.extensions.create('mcartifact', McArtifactExtension)
-        
+        def ext = project.extensions.create('mcartifact', McArtifactExtension)
         project.afterEvaluate {
-            def key = "${extension.version}_${extension.environment}"
+            def key = "${ext.version}_${ext.environment}"
             def url = "https://raw.githubusercontent.com/freehij/resources/refs/heads/main/versions.json"
             def json = new JsonSlurper().parse(new URL(url))
-            
-            if (!json[key]) {
-                throw new RuntimeException("Key $key not found in versions.json")
-            }
-            
+            if (!json[key]) throw new RuntimeException("Key $key not found in versions.json")
             def downloadUrl = json[key]
             def fileName = downloadUrl.substring(downloadUrl.lastIndexOf('/') + 1)
-            def artifactFile = new File(project.gradle.gradleUserHomeDir, ".gradle/mcartifacts/$key/$fileName")
-            
+            def cacheDir = new File(project.gradle.gradleUserHomeDir, ".gradle/mcartifacts/$key")
+            def artifactFile = new File(cacheDir, fileName)
             if (!artifactFile.exists()) {
                 artifactFile.parentFile.mkdirs()
                 artifactFile << new URL(downloadUrl).openStream()
             }
-            
-            project.dependencies.add('compileOnly', project.files(artifactFile))
-            
-            project.tasks.register('downloadMcArtifact') {
-                doLast {
-                    println "Downloaded: $artifactFile.absolutePath"
+            def dependencyFile = artifactFile
+            if (ext.environment == "server") {
+                def mergedFile = new File(cacheDir, "merged-${fileName}")
+                if (!mergedFile.exists()) {
+                    def innerPath = "META-INF/versions/${ext.version}/server-${ext.version}.jar"
+                    def tempInner = File.createTempFile("inner-server-${ext.version}", ".jar")
+                    tempInner.deleteOnExit()
+                    new ZipFile(artifactFile).withCloseable { outer ->
+                        def innerEntry = outer.getEntry(innerPath)
+                        if (!innerEntry) throw new RuntimeException("Inner jar not found at $innerPath")
+                        tempInner.withOutputStream { os ->
+                            os << outer.getInputStream(innerEntry)
+                        }
+                    }
+                    mergedFile.parentFile.mkdirs()
+                    new ZipOutputStream(new FileOutputStream(mergedFile)).withCloseable { zos ->
+                        new ZipFile(tempInner).withCloseable { innerZip ->
+                            innerZip.entries().each { entry ->
+                                if (!entry.directory) {
+                                    zos.putNextEntry(new ZipEntry(entry.name))
+                                    zos << innerZip.getInputStream(entry)
+                                    zos.closeEntry()
+                                }
+                            }
+                        }
+                        new ZipFile(artifactFile).withCloseable { outerZip ->
+                            outerZip.entries().each { entry ->
+                                if (!entry.directory && entry.name.startsWith("net/minecraft/bundler/")) {
+                                    zos.putNextEntry(new ZipEntry(entry.name))
+                                    zos << outerZip.getInputStream(entry)
+                                    zos.closeEntry()
+                                }
+                            }
+                        }
+                    }
+                    tempInner.delete()
                 }
+                dependencyFile = mergedFile
+            }
+            project.dependencies.add('compileOnly', project.files(dependencyFile))
+            project.tasks.register('downloadMcArtifact') {
+                doLast { println "Artifact available at: ${dependencyFile.absolutePath}" }
             }
         }
     }
